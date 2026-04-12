b7f3f1138062f1dc7c5218d61a8e0e55da03f431
kalet chavez <kalet.svdev@gmail.com>
2026-04-12 02:03:27 -0500

    :sparkles: Delete last excercises

--- Main.java
+++ Main.java
@@ -1,13 +1,7 @@
-import solutions01.excercise01.ExecutionLoggerApp;
 
 public class Main {
 
     public static void main(String[] args) {
-        ExecutionLoggerApp exec = new ExecutionLoggerApp();
 
-        System.out.println(exec.getUserName());
-        System.out.println(exec.getOsName());
-        System.out.println(exec.getJavaVersion());
-        System.out.println(exec.getDateAndHour());
     }
 }
